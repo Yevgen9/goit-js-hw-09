c64dce14fc78818f309d970607d291d8b54516b9
Yevgen9 <110126038+Yevgen9@users.noreply.github.com>
2024-02-23 15:49:09 +0000

    Deploying to gh-pages from @ Yevgen9/goit-js-hw-09@f039a572e4e51bd38ed0ae3000a919c7eb04222d 🚀

---
+++ 01-color-switcher.e2eb351c.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.setAttribute("disabled","true"),t.addEventListener("click",(()=>{t.setAttribute("disabled","true"),e.removeAttribute("disabled");const r=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3);e.addEventListener("click",(()=>{e.setAttribute("disabled","true"),t.removeAttribute("disabled"),clearInterval(r)}))}));
+//# sourceMappingURL=01-color-switcher.e2eb351c.js.map
